2b25ec5bb4a75fbdffe5860e9bfd7dca167438d4
yaominguo <missgmy@yahoo.com>
2020-04-13 13:59:34 +0800

    Init Project

---
+++ src/components/layout.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    Layout
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Layout',
+  data() {
+    return {}
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/router/routes.js
@@ -0,0 +1,9 @@
+const Layout = () => import('@/components/layout.vue')
+
+export default [
+  {
+    path: '/',
+    name: 'Layout',
+    component: Layout
+  }
+]
